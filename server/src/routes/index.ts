9d5725df5f7b1b73923b2a89317c513b66d98299
MINJOOOONG <zzz1577@naver.com>
2026-06-20 18:47:59 +0900

    친구 주식 유저 라우트 등록

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
+import friendRoutes from './friend.routes';
+import stockRoutes from './stock.routes';
+import userRoutes from './user.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-
-// Phase 2에서 추가
-// router.use('/friends', friendRoutes);
-// router.use('/stocks', stockRoutes);
-// router.use('/users', userRoutes);
+router.use('/friends', friendRoutes);
+router.use('/stocks', stockRoutes);
+router.use('/users', userRoutes);
 
 // Phase 3에서 추가
 // router.use('/battles', battleRoutes);
