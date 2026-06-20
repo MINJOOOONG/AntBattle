7e7a79b9e0a81a3ad55f8a2e99ec73aa1fba85d2
MINJOOOONG <zzz1577@naver.com>
2026-06-20 19:17:09 +0900

    배틀 라우트 등록

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from './auth.routes';
 import friendRoutes from './friend.routes';
 import stockRoutes from './stock.routes';
 import userRoutes from './user.routes';
+import battleRoutes from './battle.routes';
 
 const router = Router();
 
@@ -10,9 +11,7 @@ router.use('/auth', authRoutes);
 router.use('/friends', friendRoutes);
 router.use('/stocks', stockRoutes);
 router.use('/users', userRoutes);
-
-// Phase 3에서 추가
-// router.use('/battles', battleRoutes);
+router.use('/battles', battleRoutes);
 
 // Phase 4에서 추가
 // router.use('/shop', shopRoutes);
