cba2ef2b311d6bd3725aac11a317d6ff251dfd59
MINJOOOONG <zzz1577@naver.com>
2026-06-22 14:53:42 +0900

    ClayAntCharacter 블링크 애니메이션 제거 및 구조 단순화

--- mobile/src/components/ant/ClayAntCharacter.tsx
+++ mobile/src/components/ant/ClayAntCharacter.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from 'react';
 import {
   Animated,
   Easing,
-  Image,
   ImageSourcePropType,
   StyleSheet,
   View,
@@ -47,7 +46,6 @@ export default function ClayAntCharacter({
       : SIZE_MAP[size];
   const selectedVariant = variant ?? mood ?? 'idle';
   const floatAnim = useRef(new Animated.Value(0)).current;
-  const blinkAnim = useRef(new Animated.Value(0)).current;
   void rankScore;
 
   useEffect(() => {
@@ -72,42 +70,6 @@ export default function ClayAntCharacter({
     return () => animation.stop();
   }, [floatAnim]);
 
-  useEffect(() => {
-    const blink = Animated.loop(
-      Animated.sequence([
-        Animated.delay(2200),
-        Animated.timing(blinkAnim, {
-          toValue: 1,
-          duration: 90,
-          easing: Easing.out(Easing.quad),
-          useNativeDriver: true,
-        }),
-        Animated.timing(blinkAnim, {
-          toValue: 0,
-          duration: 130,
-          easing: Easing.in(Easing.quad),
-          useNativeDriver: true,
-        }),
-        Animated.delay(1500),
-        Animated.timing(blinkAnim, {
-          toValue: 1,
-          duration: 80,
-          easing: Easing.out(Easing.quad),
-          useNativeDriver: true,
-        }),
-        Animated.timing(blinkAnim, {
-          toValue: 0,
-          duration: 120,
-          easing: Easing.in(Easing.quad),
-          useNativeDriver: true,
-        }),
-      ])
-    );
-
-    blink.start();
-    return () => blink.stop();
-  }, [blinkAnim]);
-
   const translateY = floatAnim.interpolate({
     inputRange: [0, 1],
     outputRange: [1.5, -2.5],
@@ -116,81 +78,19 @@ export default function ClayAntCharacter({
     inputRange: [0, 1],
     outputRange: [0.99, 1.015],
   });
-  const eyeSize = imageSize * 0.072;
-  const eyeTop = imageSize * 0.45;
-  const eyeCoverSize = eyeSize * 1.35;
-  const blinkLineScale = blinkAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0.7, 1],
-  });
+
   return (
     <View style={[styles.container, { width: imageSize, height: imageSize }]}>
-      <Animated.View
+      <Animated.Image
+        source={MASCOT_IMAGES[selectedVariant]}
         style={[
-          styles.animatedLayer,
+          styles.image,
           {
             transform: [{ translateY }, { scale }],
           },
         ]}
-      >
-        <Image
-          source={MASCOT_IMAGES[selectedVariant]}
-          style={styles.image}
-          resizeMode="contain"
-        />
-        <View pointerEvents="none" style={styles.faceLayer}>
-          <Animated.View
-            style={[
-              styles.eyeCover,
-              {
-                left: imageSize * 0.36,
-                top: eyeTop - eyeSize * 0.16,
-                width: eyeCoverSize,
-                height: eyeCoverSize * 0.86,
-                borderRadius: eyeCoverSize / 2,
-                opacity: blinkAnim,
-              },
-            ]}
-          />
-          <Animated.View
-            style={[
-              styles.eyeCover,
-              {
-                left: imageSize * 0.555,
-                top: eyeTop - eyeSize * 0.16,
-                width: eyeCoverSize,
-                height: eyeCoverSize * 0.86,
-                borderRadius: eyeCoverSize / 2,
-                opacity: blinkAnim,
-              },
-            ]}
-          />
-          <Animated.View
-            style={[
-              styles.blinkLine,
-              {
-                left: imageSize * 0.382,
-                top: eyeTop + eyeSize * 0.28,
-                width: eyeSize * 0.78,
-                transform: [{ scaleX: blinkLineScale }],
-                opacity: blinkAnim,
-              },
-            ]}
-          />
-          <Animated.View
-            style={[
-              styles.blinkLine,
-              {
-                left: imageSize * 0.577,
-                top: eyeTop + eyeSize * 0.28,
-                width: eyeSize * 0.78,
-                transform: [{ scaleX: blinkLineScale }],
-                opacity: blinkAnim,
-              },
-            ]}
-          />
-        </View>
-      </Animated.View>
+        resizeMode="contain"
+      />
     </View>
   );
 }
@@ -201,33 +101,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     overflow: 'visible',
   },
-  animatedLayer: {
-    height: '100%',
-    width: '100%',
-  },
   image: {
     width: '100%',
     height: '100%',
-  },
-  faceLayer: {
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-    right: 0,
-    top: 0,
-  },
-  eye: {
-    backgroundColor: '#3C2E28',
-    position: 'absolute',
-  },
-  eyeCover: {
-    backgroundColor: '#C7AA93',
-    position: 'absolute',
-  },
-  blinkLine: {
-    backgroundColor: '#7B6256',
-    borderRadius: 999,
-    height: 1.5,
-    position: 'absolute',
   },
 });
