2efdce14e4b5392dd263867d49790a98129764f2
MINJOOOONG <zzz1577@naver.com>
2026-06-24 23:55:50 +0900

    user.controller SAFE_USER_SELECT 공통 유틸 import로 교체

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -4,30 +4,9 @@ import { AuthRequest } from '../types';
 import { antBeanService } from '../services/ant-bean.service';
 import { NotFoundError } from '../utils/errors';
 import { getRankName } from '../utils/rank';
+import { FULL_USER_SELECT } from '../utils/user-select';
 
 const prisma = new PrismaClient();
-
-const SAFE_USER_SELECT = {
-  id: true,
-  email: true,
-  nickname: true,
-  handle: true,
-  rankScore: true,
-  winCount: true,
-  loseCount: true,
-  drawCount: true,
-  currentWinStreak: true,
-  bestWinStreak: true,
-  battleTickets: true,
-  equippedHatId: true,
-  equippedGlassesId: true,
-  equippedExpressionId: true,
-  equippedOutfitId: true,
-  equippedBackgroundId: true,
-  equippedTitleId: true,
-  createdAt: true,
-  updatedAt: true,
-} as const;
 
 export class UserController {
   async getMyProfile(req: AuthRequest, res: Response, next: NextFunction): Promise<void> {
@@ -35,7 +14,7 @@ export class UserController {
       const id = req.userId!;
       const user = await prisma.user.findUnique({
         where: { id },
-        select: SAFE_USER_SELECT,
+        select: FULL_USER_SELECT,
       });
 
       if (!user) {
@@ -58,7 +37,7 @@ export class UserController {
       const id = req.params.id as string;
       const user = await prisma.user.findUnique({
         where: { id },
-        select: SAFE_USER_SELECT,
+        select: FULL_USER_SELECT,
       });
 
       if (!user) {
